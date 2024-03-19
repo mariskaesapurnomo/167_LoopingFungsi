51d24f46d3722d7e66030faaefea6c40ecd085a6
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-19 10:16:02 +0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int i;
+    int arr[5];
+
+}
