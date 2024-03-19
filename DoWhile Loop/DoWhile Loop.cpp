0299981911f7655265fa05c629182024c27e74b4
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-19 11:03:55 +0700

    membuat Do While Loop

---
+++ DoWhile Loop/DoWhile Loop.cpp
@@ -0,0 +1,25 @@
+// DoWhile Loop.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	srand(time(0));
+
+	x = rand() % 10;
+
+	cout << "Nilai awal : " << x << endl;
+
+	do
+	{
+		cout << "Perulangan Do..While" << x << endl;
+		x = rand() % 10;
+
+		cout << "Nilai X : " << x << endl;
+	} while (x > 4);
+   
+}
+
